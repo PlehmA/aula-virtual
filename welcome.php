06fe5f69af512e8919b7620aa057649c634ce7cd
Polaken <polachgg@gmail.com>
2016-11-07 13:25:16 -0300

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if ($_SESSION['logeado'])
+{
+	echo "<h1 style=text-align:center;>¡Bienvenidos!</h1>";
+	echo "</br>";
+	echo "¿Como andas " .$_SESSION['user']."?";
+	echo "</br>";
+	echo "Horario de conexion: ". $_SESSION['time'];
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<hr>
+		<a href='inicio.php'>LOGOUT</a>
+		<br>
+		<hr>
+		<a href='list_productos.php'>Lista productos</a>
+	</body>
+</html>
