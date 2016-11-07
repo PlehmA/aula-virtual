41256dc0ad5d584533dc1117a363abdd2af769c9
Polaken <polachgg@gmail.com>
2016-11-07 13:32:56 -0300

    Add files via upload

--- includes/bdd.php
+++ includes/bdd.php
@@ -2,8 +2,9 @@
 function crearConexion() {
 	$config = parse_ini_file("database.ini");
 	$con = new mysqli ($config[SERVER],$config[USER],$config[PASSWORD],$config[NAMEBDD]);
-	if ($con->connect_errno > 0);
+	if ($con->connect_errno > 0){
 	die ( "Error en la conexión" );
+}
 	return $con;
 }
 ?>
