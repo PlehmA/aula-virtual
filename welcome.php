b4a64871b3d767b7e90e46a91268a7bbf3b0bc7f
Polaken <polachgg@gmail.com>
2016-11-08 12:51:04 -0300

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -14,12 +14,15 @@ if ($_SESSION['logeado'])
 	<head>
 		<meta charset="utf-8">
 		<title></title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	</head>
 	<body>
 		<hr>
-		<a href='inicio.php'><button type="submit" name="button" class="btn btn-danger">Logout</button></a>
+		<a href='inicio.php' <?php session_destroy(); ?>><button type="submit" name="button" class="btn btn-danger">Logout</button></a>
 		<br>
 		<hr>
 		<a href='list_productos.php'>Lista productos</a>
+		<script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</body>
 </html>
