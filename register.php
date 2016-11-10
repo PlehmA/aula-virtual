6c2bc6113349fea94ba7f0004382304f6e08fb6b
Polaken <polachgg@gmail.com>
2016-11-10 12:22:30 -0300

    Add files via upload

--- register.php
+++ register.php
@@ -1,18 +1,21 @@
 <?php
-session_start();
-header('Content-Type: text/html;charset=utf-8');
-include_once 'includes/bdd.php';
-$con = crearConexion();
-$con -> set_charset("utf-8");
-$id_usr=$_POST['id_usuario']
-$nombre=$_POST['name'];
-$usuario=$_POST['username'];
-$pass=$_POST['password']===$repass=$_POST['repassword'];
-$email=$_POST['email'];
-$sql='INSERT INTO usuarios(id_usuario,name,username,password,email) VALUES (?,?,?,?,?)';
-$stmt=$con->prepare($sql);
-$stmt->bind_param('issss', $id_usr, $nombre, $usuario, $pass, $email);
-$stmt->execute();
-$con->close();
-header('Location:bienvenido.html');
+if ($_POST) {
+  session_start();
+  header('Content-Type: text/html;charset=utf-8');
+  include_once 'includes/bdd.php';
+  $con = crearConexion();
+  $con -> set_charset("utf-8");
+  $id_usr=$_POST['id_usuario'];
+  $nombre=$_POST['name'];
+  $usuario=$_POST['username'];
+  $pass=$_POST['password']===$repass=$_POST['repassword'];
+  $email=$_POST['email'];
+  $sql='INSERT INTO usuarios(id_usuario,name,username,password,email) VALUES (?,?,?,?,?)';
+  $stmt=$con->prepare($sql);
+  $stmt->bind_param('issss', $id_usr, $nombre, $usuario, $pass, $email);
+  $stmt->execute();
+  $con->close();
+  header('Location:bienvenido.html');
+}
+
 ?>
