52be4bf0418b07c85a5c78c069c135d1b003acea
Polaken <polachgg@gmail.com>
2016-11-17 13:27:27 -0300

    Update user_login.php

--- user_login.php
+++ user_login.php
@@ -3,31 +3,30 @@ var_dump(error_log(message));
 include_once ('includes/bdd.php');
 header('Content-Type: text/html;charset-UTF-8');
 $usuario=$_POST['username'];
-$pass=$_POST['password'];
+$password=$_POST['password'];
 $con=crearConexion();
 $con->set_charset("UTF-8");
-
+$md5_pass = md5($password);
 $sql="SELECT id_usuario,username,password,email FROM usrs_cmns WHERE username = '$usuario' AND password = '$md5_pass'";
 $stmt = $con->prepare($sql);
-$stmt->bind_param('ss', $usuario, $pass);
+$stmt->bind_param('ss', $usuario, $password);
 $stmt->execute();
 $result2=$con->query($sql);
 $row=$result2->fetch_assoc();
 if ($row[$sql]==0)
 {
 
-	echo "<script>alert ('Ingreso invalido al sistema!')</script>";
-    	echo "<script>window.location.assign('index.php')</script>";
+echo "<script>alert ('Ingreso invalido al sistema!')</script>";
+echo "<script>window.location.assign('index.php')</script>";
 
 
 }
 else
 {
-			session_start();
-			$_SESSION['username']=$usuario;
-			$_SESSION['logeado']=true;
-			$con->close();
-      header('Location:userapp.php');
+session_start();
+$_SESSION['username']=$usuario;
+$_SESSION['logeado']=true;
+header('Location:userapp.php');
 			};
 
  ?>
